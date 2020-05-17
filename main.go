8301175eb8d7f9f9d15cd8a9420b24686ac19391
cjw <cwentsai@gmail.com>
2020-05-17 16:35:18 +0800

    Rearrange code

--- main.go
+++ main.go
@@ -23,37 +23,24 @@ func (gs *GoogleSheet) Init(spreadsheetId string) {
 	}
 }
 
-func main() {
-	spreadsheetId := "12Luq-VG23UxdcIhfmNCtW_BL4fpHPUwUK-cfwRJyJx0"
+func (gs *GoogleSheet) Read(readRange string) [][]interface{} {
+	resp, err := gs.srv.Spreadsheets.Values.Get(gs.spreadsheetId, readRange).Do()
 
-	var gs GoogleSheet
-	gs.Init(spreadsheetId)
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
 
-	// https://docs.google.com/spreadsheets/d/12Luq-VG23UxdcIhfmNCtW_BL4fpHPUwUK-cfwRJyJx0/edit
-	readRange := "Table!A2:E"
-
-	values := gs.Read(readRange)
-	fmt.Println(values)
-
-	var writeValues [][]interface{}
-	row := []interface{}{"AAA", "BBB"}
-	writeValues = append(writeValues, row)
-	gs.Write("Table", writeValues)
-
-	var updateValues [][]interface{}
-	row = []interface{}{"BBB", "CCC", "DDD"}
-	updateValues = append(updateValues, row)
-	gs.Update("Table!A3", updateValues)
-
-	clearRange := "Table!A3:E"
-	gs.Clear(clearRange)
+	return resp.Values
 }
 
-func (gs *GoogleSheet) Clear(clearRange string) {
-	// rb has type *ClearValuesRequest
-	rb := &sheets.ClearValuesRequest{}
+func (gs *GoogleSheet) Write(writeRange string, values [][]interface{}) {
+	valueInputOption := "RAW"
+	rb := &sheets.ValueRange{
+		MajorDimension: "ROWS",
+		Values:         values,
+	}
 
-	_, err := gs.srv.Spreadsheets.Values.Clear(gs.spreadsheetId, clearRange, rb).Do()
+	_, err := gs.srv.Spreadsheets.Values.Append(gs.spreadsheetId, writeRange, rb).ValueInputOption(valueInputOption).Do()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,25 +59,37 @@ func (gs *GoogleSheet) Update(updateRange string, updateValues [][]interface{}) 
 	}
 }
 
-func (gs *GoogleSheet) Write(writeRange string, values [][]interface{}) {
-	valueInputOption := "RAW"
-	rb := &sheets.ValueRange{
-		MajorDimension: "ROWS",
-		Values:         values,
-	}
+func (gs *GoogleSheet) Clear(clearRange string) {
+	// rb has type *ClearValuesRequest
+	rb := &sheets.ClearValuesRequest{}
 
-	_, err := gs.srv.Spreadsheets.Values.Append(gs.spreadsheetId, writeRange, rb).ValueInputOption(valueInputOption).Do()
+	_, err := gs.srv.Spreadsheets.Values.Clear(gs.spreadsheetId, clearRange, rb).Do()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (gs *GoogleSheet) Read(readRange string) [][]interface{} {
-	resp, err := gs.srv.Spreadsheets.Values.Get(gs.spreadsheetId, readRange).Do()
+func main() {
+	// https://docs.google.com/spreadsheets/d/12Luq-VG23UxdcIhfmNCtW_BL4fpHPUwUK-cfwRJyJx0/edit
+	spreadsheetId := "12Luq-VG23UxdcIhfmNCtW_BL4fpHPUwUK-cfwRJyJx0"
 
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
-	}
+	var gs GoogleSheet
+	gs.Init(spreadsheetId)
 
-	return resp.Values
+	readRange := "Table!A2:E"
+	values := gs.Read(readRange)
+	fmt.Println(values)
+
+	var writeValues [][]interface{}
+	row := []interface{}{"AAA", "BBB"}
+	writeValues = append(writeValues, row)
+	gs.Write("Table", writeValues)
+
+	var updateValues [][]interface{}
+	row = []interface{}{"BBB", "CCC", "DDD"}
+	updateValues = append(updateValues, row)
+	gs.Update("Table!A3", updateValues)
+
+	clearRange := "Table!A3:E"
+	gs.Clear(clearRange)
 }
